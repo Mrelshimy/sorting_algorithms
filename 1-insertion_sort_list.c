fce53f1987468949580009d43939255a335031b9
Mrelshimy <mraafat.elsayed@gmail.com>
2023-12-14 13:28:47 +0200

    update

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,17 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - C implementation of insertion sort algorithm
+ * @list: Doubly linked list to sort
+ *
+*/
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *hold = list, *scan = list, *tmp;
+
+    hold = hold->next;
+    tmp = hold->n;
+    
+
+}
---
+++ 2-selection_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * selection_sort - C implementation of selection sort algorithm
+ * @array: input array to be sorted
+ * @size: array size
+ *
+*/
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min, tmp;
+
+	for (i = 0; i < size; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+
+		if (min != i)
+		{
+			tmp = array[min];
+			array[min] = array[i];
+			array[i] = tmp;
+			print_array(array, size);
+		}
+	}
+}
