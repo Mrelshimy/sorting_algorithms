ae1cbd090329904572eb6d239597dfc8a2baa12c
Mrelshimy <mraafat.elsayed@gmail.com>
2023-12-15 18:03:03 +0200

    insertion sort file

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,17 +1,48 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - C implementation of insertion sort algorithm
- * @list: Doubly linked list to sort
+ * insertion_sort - C implementation of insertion sort algorithm
+ * @array: input array to be sorted
+ * @size: array size
  *
-*/
+ */
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *hold = list, *scan = list, *tmp;
+	listint_t *n, *m;
 
-    hold = hold->next;
-    tmp = hold->n;
-    
+	if (!list || !*list || !(*list)->next)
+		return;
 
+	n = (*list)->next;
+
+	while(n)
+	{
+		m = n;
+		n = n->next;
+		while (m && m->prev)
+		{
+			if (m->prev->n > m->n)
+			{
+				node_swap(m->prev, m);
+				if (m->prev == NULL)
+					*list = m;
+				print_list((const listint_t *)*list);
+			}
+			else
+				m = m->prev;
+		}
+	}
+}
+
+void node_swap(listint_t *x, listint_t *y)
+{
+	if (x->prev)
+		x->prev->next = y;
+	if (y->next)
+		y->next->prev = x;
+	x->next = y->next;
+	y->prev = x->prev;
+	x->prev = y;
+	y->next = x;
 }
