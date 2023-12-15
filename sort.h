ad9f86b1cdfe0c462295ec40e195857eae9c40e1
Mrelshimy <mraafat.elsayed@gmail.com>
2023-12-15 03:00:10 +0200

    update

--- sort.h
+++ sort.h
@@ -25,8 +25,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void swap(int* a, int* b, int *array, size_t size);
-int partition (int arr[], int low, int high, int *array, size_t size);
-void quickSort(int arr[], int low, int high, int *array, size_t size);
+void quicksort(int *array, int start, int end, size_t size);
+int partition(int *array, int start, int end, size_t size);
 
 #endif
