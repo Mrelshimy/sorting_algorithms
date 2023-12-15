c2576fdb380219835b9fcacaac059019c5c38218
Mrelshimy <mraafat.elsayed@gmail.com>
2023-12-15 11:38:42 +0200

    update for shell sort

--- sort.h
+++ sort.h
@@ -27,5 +27,7 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void quicksort(int *array, int start, int end, size_t size);
 int partition(int *array, int start, int end, size_t size);
+void shell_sort(int *array, size_t size);
+size_t get_gap(size_t size);
 
 #endif
