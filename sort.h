8565de375757941783c197fb84c9ff8963bbbcb9
Mrelshimy <mraafat.elsayed@gmail.com>
2023-12-15 18:02:48 +0200

    update

--- sort.h
+++ sort.h
@@ -29,5 +29,5 @@ void quicksort(int *array, int start, int end, size_t size);
 int partition(int *array, int start, int end, size_t size);
 void shell_sort(int *array, size_t size);
 size_t get_gap(size_t size);
-
+void node_swap(listint_t *x, listint_t *y);
 #endif
