aaff3c4c67f41e35d299ff92d875028749878565
Mrelshimy <mraafat.elsayed@gmail.com>
2023-12-15 02:28:41 +0200

    update file 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,40 +1,85 @@
 #include "sort.h"
 
-void swap(int* a, int* b, int *array, size_t size) 
+/**
+ * quick_sort - C implementation of selection sort algorithm
+ * @array: input array to be sorted
+ * @size: array size
+ *
+ */
+
+void quick_sort(int *array, size_t size)
 {
-    int t;
+	if (array == NULL || size == 0)
+		return;
 
-    t = *a;
-    *a = *b;
-    *b = t;
-    print_array((const int*)array, size);
+	quickSort(array, 0, size - 1, array, size);
 }
 
-int partition (int arr[], int low, int high, int *array, size_t size) {
-    int pivot = arr[high];  
-    int i = (low - 1), j;  
+/**
+ * swap - swap 2 variables values
+ * @array: input array to be printed
+ * @size: array size
+ * @a: first var
+ * @b: second var
+ *
+ */
 
-    for (j = low; j <= high- 1; j++) {
-        if (arr[j] < pivot) {
-            i++;  
-            swap(&arr[i], &arr[j], array, size);
-        }
-    }
-    swap(&arr[i + 1], &arr[high], array, size);
-    return (i + 1);
-}
-
-void quickSort(int arr[], int low, int high, int *array, size_t size) {
-    if (low < high) {
-        int pi;
-        
-        pi = partition(arr, low, high, array, size);
-        quickSort(arr, low, pi - 1, array, size);
-        quickSort(arr, pi + 1, high, array, size);
-    }
-}
-
-void quick_sort(int *array, size_t size) 
+void swap(int *a, int *b, int *array, size_t size)
 {
-    quickSort(array, 0, size-1, array, size);
+	int t;
+
+	t = *a;
+	*a = *b;
+	*b = t;
+	print_array(array, size);
+}
+
+/**
+ * partition - partitioning of array
+ * @array: input array to be sorted
+ * @size: array size
+ * @arr: array
+ * @low: lower bound
+ * @high: higher bound
+ *
+ * Return: pivot location
+ */
+
+int partition(int arr[], int low, int high, int *array, size_t size)
+{
+	int pivot = arr[high];
+	int i = (low - 1), j;
+
+	for (j = low; j <= high - 1; j++)
+	{
+		if (arr[j] < pivot)
+		{
+			i++;
+			swap(&arr[i], &arr[j], array, size);
+		}
+	}
+	swap(&arr[i + 1], &arr[high], array, size);
+	return (i + 1);
+}
+
+/**
+ * quickSort - sorting of array fn
+ * @array: input array to be sorted
+ * @size: array size
+ * @arr: array
+ * @low: lower bound
+ * @high: higher bound
+ *
+ */
+
+void quickSort(int arr[], int low, int high, int *array, size_t size)
+{
+	if (low < high)
+	{
+		int pi;
+
+		pi = partition(arr, low, high, array, size);
+		quickSort(arr, low, pi - 1, array, size);
+		quickSort(arr, pi + 1, high, array, size);
+	}
 }
