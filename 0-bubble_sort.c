7191cabe2f607af196077e482100248587c45dcb
Mrelshimy <mraafat.elsayed@gmail.com>
2023-12-15 02:04:28 +0200

    update bubble sort file

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,8 +12,8 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j, tmp;
 
 	if (array == NULL || size == 0)
-		return NULL;
-		
+		return (NULL);
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - 1; j++)
