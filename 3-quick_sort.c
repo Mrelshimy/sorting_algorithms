3c00273f405b18b56d8b9d431e9fafd87b15cd1a
Mrelshimy <mraafat.elsayed@gmail.com>
2023-12-16 12:47:12 +0200

    quick sort update

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -5,24 +5,28 @@
  * @array: input array to be sorted
  * @size: array size
  *
+ * Description: This function sorts an array of integers
+ *              in ascending order using the quicksort algorithm.
  */
-
 void quick_sort(int *array, size_t size)
 {
-	if (array == NULL || size == 0)
+	if (array == NULL || size <= 1)
 		return;
-	quicksort(array, 0, size - 1, size);
+
+	q_sort(array, 0, size - 1, size);
 }
 
 /**
- * quicksort - sorting of array fn
+ * q_sort - Recursive function to perform quicksort
  * @array: input array to be sorted
+ * @start: lower bound of the array or subarray
+ * @end: upper bound of the array or subarray
  * @size: array size
- * @start: lower bound
- * @end: higher bound
  *
+ * Description: This function is a helper function for quick_sort.
+ *              It recursively sorts the array or subarray using quicksort.
  */
-void quicksort(int *array, int start, int end, size_t size)
+void q_sort(int *array, int start, int end, size_t size)
 {
 	int pivot;
 
@@ -30,39 +34,65 @@ void quicksort(int *array, int start, int end, size_t size)
 		return;
 
 	pivot = partition(array, start, end, size);
-	quicksort(array, start, pivot - 1, size);
-	quicksort(array, pivot + 1, end, size);
+
+	/* Uncomment the following line if you want to see intermediate steps */
+	/* printf("After partition: "); print_array(array, size); */
+
+	q_sort(array, start, pivot - 1, size);
+	q_sort(array, pivot + 1, end, size);
 }
 
 /**
- * partition - partitioning of array
+ * partition - Partitioning of array for quicksort
  * @array: input array to be sorted
+ * @start: lower bound of the array or subarray
+ * @end: upper bound of the array or subarray
  * @size: array size
- * @start: lower bound
- * @end: higher bound
  *
  * Return: pivot location
+ *
+ * Description: This function takes the last element of the array as the pivot,
+ *              places the pivot element at its correct position in the sorted
+ *              array, and places all smaller elements to the left of the pivot
+ *              and all greater elements to the right of the pivot.
  */
 int partition(int *array, int start, int end, size_t size)
 {
 	int pivot = array[end];
-	int i = start - 1, j, tmp;
+	int i = start - 1, j;
 
-	(void) size;
 	for (j = start; j <= end - 1; j++)
 	{
 		if (array[j] < pivot)
 		{
+			var_swap(array, size, &array[j], &array[i + 1]);
 			i++;
-			tmp = array[i];
-			array[i] = array[j];
-			array[j] = tmp;
 		}
 	}
-	i++;
-	tmp = array[i];
-	array[i] = array[end];
-	array[end] = tmp;
-	print_array(array, size);
-	return (i);
+
+	var_swap(array, size, &array[i + 1], &array[end]);
+	return (i + 1);
+}
+
+/**
+ * var_swap - Swap two elements in an array
+ * @array: input array
+ * @size: array size
+ * @x: pointer to the first element
+ * @y: pointer to the second element
+ *
+ * Description: This function swaps the values of two elements in an array
+ *              and prints the array after the swap.
+ */
+void var_swap(int *array, size_t size, int *x, int *y)
+{
+	int tmp;
+
+	if (x != y)
+	{
+		tmp = *x;
+		*x = *y;
+		*y = tmp;
+		print_array(array, size);
+	}
 }
